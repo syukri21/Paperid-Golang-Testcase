fe2ff1b530daf39009104b3db040d818bd59a83c
syukri <syukrihsb148@gmail.com>
2020-08-29 03:46:25 +0700

    add signin signout signup

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -8,11 +8,37 @@ import (
 
 // Router middleware to handler routes
 func Router(g *gin.RouterGroup) {
-	g.GET("/health", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"status":   http.StatusOK,
-			"message":  "ready",
-			"database": "error",
+
+	{
+		g.POST("/signup", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"status":   http.StatusOK,
+				"message":  "ready",
+				"database": "error",
+			})
 		})
-	})
+		g.POST("/signin", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"status":   http.StatusOK,
+				"message":  "ready",
+				"database": "error",
+			})
+		})
+		g.POST("/signout", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"status":   http.StatusOK,
+				"message":  "ready",
+				"database": "error",
+			})
+		})
+	}
+	{
+		g.GET("/health", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"status":   http.StatusOK,
+				"message":  "ready",
+				"database": "error",
+			})
+		})
+	}
 }
