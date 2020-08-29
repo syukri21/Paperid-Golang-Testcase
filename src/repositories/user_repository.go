2661360f5fffaeeb357a19c7b9324434716957e8
syukri <syukrihsb148@gmail.com>
2020-08-29 17:23:50 +0700

    add createuser repository

---
+++ src/repositories/user_repository.go
@@ -0,0 +1,35 @@
+package repositories
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+	"github.com/syukri21/Paperid-Golang-Testcase/src/database/entity"
+
+	db "github.com/syukri21/Paperid-Golang-Testcase/src/database"
+)
+
+// UserRepository -> the propose of user repository is handling query for user entity
+type UserRepository struct {
+	Conn *gorm.DB
+}
+
+// GetUser -> GetUser entity schema
+type GetUser struct {
+	ID        uint       `json:"id,omitempty"`
+	Email     string     `json:"email,omitempty"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+}
+
+// UserRepositoryInstance -> user repository instance to get user table connection
+func UserRepositoryInstance() UserRepository {
+	return UserRepository{Conn: db.GetDB().Table("users")}
+}
+
+// CreateUser -> CreateUser
+func (r *UserRepository) CreateUser(user entity.User) GetUser {
+	r.Conn.Create(&user)
+	userCreated := GetUser{}
+	r.Conn.Select("id, email, deleted_at").Where("id=?").First(&userCreated)
+	return userCreated
+}
