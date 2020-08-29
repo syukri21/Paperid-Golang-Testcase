47b53b194fab611f60b4e783cfb969fbe68531f5
syukri <syukrihsb148@gmail.com>
2020-08-29 19:04:11 +0700

    add uuid

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/mitchellh/mapstructure v1.3.3
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.6.0
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
--- src/database/entity/user_entity.go
+++ src/database/entity/user_entity.go
@@ -1,10 +1,14 @@
 package entity
 
-import "time"
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
 
 // User -> user entity schema
 type User struct {
-	ID        uint       `gorm:"primary_key" json:"id"`
+	ID        uuid.UUID  `gorm:"primary_key;type:uuid" json:"id"`
 	Email     string     `gorm:"type:varchar(100);unique_index" json:"email" faker:"email"`
 	Password  string     `gorm:"type:varchar(255)" json:"password" faker:"password"`
 	JwtToken  string     `gorm:"type:varchar(255)" json:"JwtToken" faker:"jwt"`
