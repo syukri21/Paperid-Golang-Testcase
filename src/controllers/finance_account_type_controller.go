17b69d6ffbbb4c3d6084f5846b3cddee6b8e9adf
syukri <syukrihsb148@gmail.com>
2020-08-30 06:57:58 +0700

    add FinanceAccountTypeController

---
+++ src/controllers/finance_account_type_controller.go
@@ -0,0 +1,10 @@
+package controllers
+
+// FinanceAccountTypeController -> FinanceAccountTypeController
+type FinanceAccountTypeController struct {
+}
+
+// FinanceAccountTypeControllerInstance -> FinanceAccountTypeControllerInstance
+func FinanceAccountTypeControllerInstance() FinanceAccountTypeController {
+	return FinanceAccountTypeController{}
+}
