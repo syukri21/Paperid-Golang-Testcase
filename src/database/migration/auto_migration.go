858c05cef67a11f138e43f5576290ce66fe8f0bb
syukri <syukrihsb148@gmail.com>
2020-08-31 00:13:11 +0700

    add foreignkey on migrate

--- src/database/migration/auto_migration.go
+++ src/database/migration/auto_migration.go
@@ -8,6 +8,26 @@ import (
 
 // AutoMigration is auto migrate database
 func AutoMigration(conn *gorm.DB) {
-	conn.AutoMigrate(entity.User{}, entity.Profile{}, entity.FinanceAccount{}, entity.FinanceTransaction{}, entity.FinanceAccountType{})
+	conn.AutoMigrate(entity.User{})
+
+	if conn.HasTable(&entity.User{}) {
+		conn.AutoMigrate(entity.Profile{})
+		conn.Model(&entity.Profile{}).AddForeignKey("user_id", "users(id)", "CASCADE", "CASCADE")
+
+		conn.AutoMigrate(entity.FinanceAccount{})
+		conn.Model(&entity.FinanceAccount{}).AddForeignKey("user_id", "users(id)", "CASCADE", "CASCADE")
+
+		if conn.HasTable(&entity.FinanceAccount{}) {
+			conn.AutoMigrate(entity.FinanceTransaction{})
+			conn.Model(&entity.FinanceTransaction{}).AddForeignKey("user_id", "users(id)", "CASCADE", "CASCADE")
+			conn.Model(&entity.FinanceTransaction{}).AddForeignKey("finance_account_id", "finance_accounts(id)", "CASCADE", "CASCADE")
+		}
+	}
+
+	conn.AutoMigrate(entity.FinanceAccountType{})
+	if (conn.HasTable(&entity.FinanceAccount{})) {
+		conn.Model(&entity.FinanceAccount{}).AddForeignKey("type_id", "finance_account_types(id)", "CASCADE", "CASCADE")
+	}
+
 	logrus.Info("Success running migration")
 }
