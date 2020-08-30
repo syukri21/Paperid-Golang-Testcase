e2d9bb7ef87ceffd2c6590cba4f9812caae706b5
syukri <syukrihsb148@gmail.com>
2020-08-30 07:12:03 +0700

    add finance account type

---
+++ src/repositories/finance_account_type_repository.go
@@ -0,0 +1,51 @@
+package repositories
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+	"github.com/syukri21/Paperid-Golang-Testcase/src/database/entity"
+
+	db "github.com/syukri21/Paperid-Golang-Testcase/src/database"
+)
+
+// FinanceAccountTypeRepository -> the propose of user repository is handling query for user entity
+type FinanceAccountTypeRepository struct {
+	Conn *gorm.DB
+}
+
+// FinanceAccountTypeRepositoryInstance ->
+func FinanceAccountTypeRepositoryInstance() UserRepository {
+	conn := db.GetDB().Table("finance_account_types")
+	return UserRepository{Conn: conn}
+}
+
+// GetTypes ->
+func (r *FinanceAccountTypeRepository) GetTypes() []entity.FinanceAccountType {
+	financeAccountTypes := []entity.FinanceAccountType{}
+	r.Conn.Find(&financeAccountTypes)
+	return financeAccountTypes
+}
+
+// GetTypeByID ->
+func (r *FinanceAccountTypeRepository) GetTypeByID(id int) entity.FinanceAccountType {
+	financeAccountType := entity.FinanceAccountType{}
+	r.Conn.Where("id = ?", id).First(&financeAccountType)
+	return financeAccountType
+}
+
+// UpdateType ->
+func (r *FinanceAccountTypeRepository) UpdateType(Type *entity.FinanceAccountType) (entity.FinanceAccountType, error) {
+	financeAccountType := entity.FinanceAccountType{}
+	err := r.Conn.Update(Type).Error
+	return financeAccountType, err
+}
+
+// DeleteType ->
+func (r *FinanceAccountTypeRepository) DeleteType(id int) (success bool) {
+	time := time.Now()
+	err := r.Conn.Where("id = ?", id).Update(&entity.FinanceAccountType{
+		DeletedAt: &time,
+	}).Error
+	return err == nil
+}
